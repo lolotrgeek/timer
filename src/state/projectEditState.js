f97af4d623f3f3dfd9aaa73110d2108e9ce30077
lolotrgeek <lolotrgeek@gmail.com>
2020-08-21 15:01:54 -0500

    project edit

--- src/state/projectEditState.js
+++ src/state/projectEditState.js
@@ -3,7 +3,6 @@ import { colorValid, nameValid, projectValid } from '../constants/Validators'
 import { trimSoul } from '../constants/Functions'
 import * as chain from '../data/Chains'
 import * as store from '../data/Store'
-import { editedProject } from '../data/Models'
 
 let debug = false
 let state = {
@@ -64,12 +63,32 @@ const getProject = (projectId) => {
 }
 
 const updateProject = (projectEdit) => {
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
         if (projectEdit.deleted) { projectEdit.deleted = null }
         projectEdit.edited = new Date().toString()
         debug && console.log('[react Data] Updating Project', projectEdit)
         store.set(chain.projectHistory(projectEdit.id), projectEdit)
         store.put(chain.project(projectEdit.id), projectEdit)
+        try {
+            let days = Object.keys(await getTimerDates()) // OPTIMIZE: triple mapping
+            console.log(days)
+            if (days && days.length > 0) {
+                days.forEach(async day => {
+                    let projectDates = await getProjectDates(day)
+                    console.log('projectDates: ', projectDates)
+                    projectDates.forEach(projectDate => {
+                        if (projectDate.id === projectEdit.id) {
+                            console.log('Editing projectDate: ', projectDate)
+                            store.put(chain.projectDate(day, projectEdit.id), projectEdit)
+                        }
+                    })
+                })
+            }
+        } catch (error) {
+            reject('could not update ', error)
+        }
+
+        // Callback message from UI
         // TODO: make this part of chaining?
         messenger.on(`project/${projectEdit.id}`, event => {
             if (event === projectEdit) resolve(projectEdit)
@@ -77,3 +96,48 @@ const updateProject = (projectEdit) => {
         })
     })
 }
+
+
+/**
+* 
+* @param {string} day simpledate `dd-mm-yyyy` 
+*/
+const getProjectDates = (day) => new Promise((resolve, reject) => {
+    try {
+        let result = []
+        store.chainer(chain.projectDates(day), store.app).map().on((data, key) => {
+            if (!data) {
+                debug.data && console.log('[GUN node] getProjectDates No Data Found',)
+            }
+            let foundData = trimSoul(data)
+            if (foundData.type === 'project') {
+                result.push(foundData)
+            }
+            debug.data && console.log('[GUN node] getProjectDates Data Found: ', key, foundData)
+        })
+        resolve(result)
+    } catch (err) {
+        reject(err)
+    }
+
+})
+
+/**
+ * OPTIMIZE: double mapping, can just output expected map once
+ */
+const getTimerDates = () => new Promise((resolve, reject) => {
+    try {
+        let result = {}
+        store.chainer(chain.timerDates(), store.app).map().on((data, key) => {
+            if (!data) {
+                debug.data && console.log('[GUN node] getTimerDates No Data Found',)
+            }
+            let foundData = trimSoul(data)
+            result[key] = foundData
+            debug.data && console.log('[GUN node] getTimerDates Data Found: ', key, foundData)
+        })
+        resolve(result)
+    } catch (err) {
+        reject(err)
+    }
+})
