25ba399b9bae8c10633a02de97ff3cbab84aa008
lolotrgeek <lolotrgeek@gmail.com>
2020-09-09 09:29:45 -0500

    Projects styles

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -29,9 +29,9 @@ export default function Projects({ useHistory, useParams }) {
 
     const renderRow = ({ item }) => {
         return (
-            <View style={{ flexDirection: 'row', margin: 10, width: '100%' }}>
+            <View style={{ flexDirection: 'row', margin: 10, width: '100%', maxWidth: 400, alignItems: 'center', justifyContent: 'space-evenly' }}>
                 <View style={{ width: '50%' }}>
-                    <Text onPress={() => history.push(projectlink(item.id))} style={{ color: 'red' }}>{item.name}</Text>
+                    <Text onPress={() => history.push(projectlink(item.id))} style={{ color: item.color ? item.color : 'black' }}>{item.name}</Text>
                 </View>
                 <View style={{ width: '25%' }}>
                     <Button title='start' onPress={() => {
@@ -44,17 +44,21 @@ export default function Projects({ useHistory, useParams }) {
     };
 
     const HeaderButtons = () => (
-        <View style={{ flexDirection: 'row', margin: 10 }}>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', maxWidth: 400,  }}>
             <Button title='Add Timers' onPress={() => messenger.emit('GenerateTimers', { projects: projects })} />
             <Button title='Trash' onPress={() => history.push(projectTrashlink())} />
         </View>
     )
     const Header = () => (
         <View style={styles.header}>
+            <Text style={{fontSize: 30}}>Projects</Text>
+        </View>
+    )
+    const Footer = () => (
+        <View style={{ position: 'absolute', bottom: 0, padding: 10, width: '100%', backgroundColor: 'white', zIndex: 999999, flexDirection: 'column', height: 50 }}>
             <HeaderButtons />
         </View>
     )
-
     return (
         <SafeAreaView style={styles.container}>
             <Header />
@@ -72,20 +76,20 @@ export default function Projects({ useHistory, useParams }) {
                     refreshing={refresh}
                 />
             </View>
-
+            <Footer />
         </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
     header: {
-        marginTop: 50,
         flexDirection: 'row',
         padding: 10,
         width: '100%',
         backgroundColor: 'white',
         flexDirection: 'column'
     },
+
     container: {
         flex: 1,
         backgroundColor: '#fff',
