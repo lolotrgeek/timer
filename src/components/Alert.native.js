ca203fdc325c61cef1a039ec75f7867ee9683a74
lolotrgeek <lolotrgeek@gmail.com>
2020-09-30 16:05:46 -0500

    remove native alert component

--- src/components/Alert.native.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { ToastAndroid } from "react-native";
-
-/**
- * @param {*} message
- */
-export const Alert = (message, type) => ToastAndroid.showWithGravity(message, ToastAndroid.SHORT, ToastAndroid.BOTTOM);
--- src/hooks/useAlert.native.js
+++ src/hooks/useAlert.native.js
@@ -1,6 +1,4 @@
-import {Alert} from '../components/Alert.native'
-
-// Empty Function
+// Empty Function as a courtesy to DOM rendering
 // Alerts happen from the state generator service and the native layer
 // More performant since it cuts out react as middle man
 export function useAlert() { 
