a53e7fef9c88d7fafb454929d09b5e99662c50e7
lolotrgeek <lolotrgeek@gmail.com>
2020-10-01 06:37:15 -0500

    screen rotation lock

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,6 +1,8 @@
 import React, { createContext } from 'react'
 import getStyleSheet from '../styles/mainStyles'
 
+// theme and styles being set, here
+// TODO: make this dynamic for settings
 const theme = getStyleSheet('dark')
 
 const ThemeContext = createContext(theme)
